c8b1f693673379959d3c4eb492dcaa524cf8f897
mina samy lamhy <m.lamhy02598@dmu.edu.eg>
2024-08-22 17:52:50 +0300

    chore: Update Work.scss to increase width of work items

--- portfolio-front/src/container/Work/Work.jsx
+++ portfolio-front/src/container/Work/Work.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { SiAdobe, SiCodeforces } from "react-icons/si";
 import { motion } from "framer-motion";
+import { FaFigma } from "react-icons/fa";
+import { FaWordpress } from "react-icons/fa";
+import { MdDoNotDisturb } from "react-icons/md";
+import { IoLogoVercel } from "react-icons/io5";
 
-import { AppWrap,MotionWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
-import "./Work.scss";
-
 import "./Work.scss";
 
 const Work = () => {
@@ -14,7 +16,6 @@ const Work = () => {
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
-
 
   useEffect(() => {
     const query = '*[_type == "works"]';
@@ -27,41 +28,48 @@ const Work = () => {
 
   const handelWorkFilter = (item) => {
     setActiveFilter(item);
-    setAnimateCard([{y: 100, opacity: 0}]);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
-      if(item === "All"){
+      if (item === "All") {
         setFilterWork(works);
-      }else{
+      } else {
         setFilterWork(works.filter((work) => work.tags.includes(item)));
       }
     }, 500);
-
   };
 
   return (
     <>
       <h2 className="head-text">
-        my creative 
+        my creative
         <span> portfolio </span>
         section
       </h2>
 
       <div className="app__work-filter">
-        {["UI/UX", "Web App", "Mobile App", "React Js", "Database", "Problem Solving", "All"].map(
-          (item, index) => (
-            <div
-              key={item + index}
-              onClick={() => handelWorkFilter(item)}
-              className={`app__work-filter-item app__flex p-text ${
-                activeFilter === item ? "item-active" : ""
-              }`}
-            >
-              {item}
-            </div>
-          )
-        )}
+        {[
+          "UI/UX",
+          "Web App",
+          "WordPress",
+          "Mobile App",
+          "React Js",
+          "Next Js",
+          "Database",
+          "Problem Solving",
+          "All",
+        ].map((item, index) => (
+          <div
+            key={item + index}
+            onClick={() => handelWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
 
       <motion.div
@@ -76,14 +84,17 @@ const Work = () => {
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
                 className="app__work-hover app__flex"
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
-
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -93,15 +104,30 @@ const Work = () => {
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex mina"
                   >
-                    {`${work.iconName}` === "AiFillGithub" ? <AiFillGithub /> : `${work.iconName}` === "SiCodeforces" ? <SiCodeforces /> : `${work.iconName}` === "SiAdobe" ? <SiAdobe /> : <AiFillGithub /> }
+                    {`${work.iconName}` === "AiFillGithub" ? (
+                      <AiFillGithub />
+                    ) : `${work.iconName}` === "SiCodeforces" ? (
+                      <SiCodeforces />
+                    ) : `${work.iconName}` === "SiAdobe" ? (
+                      <SiAdobe />
+                    ) : `${work.iconName}` === "FaFigma" ? (
+                      <FaFigma />
+                    ) : `${work.iconName}` === "FaWordpress" ? (
+                      <FaWordpress />
+                    ) : `${work.iconName}` === "MdDoNotDisturb" ? (
+                      <MdDoNotDisturb />
+                    ) : `${work.iconName}` === "IoLogoVercel" ? (
+                      <IoLogoVercel />
+                    ) : (
+                      <AiFillGithub />
+                    )}
                   </motion.div>
                 </a>
               </motion.div>
-
             </div>
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
@@ -113,9 +139,7 @@ const Work = () => {
                 <p className="p-text">{work.tags[0]}</p>
               </div>
             </div>
-
           </div>
-
         ))}
       </motion.div>
     </>
